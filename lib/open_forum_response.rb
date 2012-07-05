44df361d6a167f04f622ab6f8859e261764adee7
nodanaonlyzuul <stephen@eastmedia.com>
2012-07-05 16:56:35 -0400

    method_missing should be defined in OpenCascade, not OpenForumResponse

--- lib/open_forum_response.rb
+++ lib/open_forum_response.rb
@@ -3,9 +3,11 @@ require 'hashery'
 
 class OpenForumResponse < Hashery::OpenCascade
 
-  def method_missing(method, *args, &block)
-    ugly_camel_name = ActiveSupport::Inflector.classify(method).to_sym
-    (self.keys.include?(ugly_camel_name)) ? self[ugly_camel_name] : super
+  class Hashery::OpenCascade
+    def method_missing(method, *args, &block)
+      ugly_camel_name = ActiveSupport::Inflector.classify(method).to_sym
+      (self.keys.include?(ugly_camel_name)) ? self[ugly_camel_name] : super
+    end
   end
 
 end
