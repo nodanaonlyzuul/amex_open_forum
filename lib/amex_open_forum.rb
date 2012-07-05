90d986d09aedf2b8271e03477219c2f061e11b6e
nodanaonlyzuul <stephen@eastmedia.com>
2012-07-05 15:18:51 -0400

    Implement most_recent, most_viewed, most_commented, most_shared, article_details, and video_details methods

--- lib/amex_open_forum.rb
+++ lib/amex_open_forum.rb
@@ -18,7 +18,6 @@ module AmexOpenForum
   attr_accessor :open_forum_api_key
 
   def most_recent(options = {})
-
     unless @most_recent
       @most_recent          = OpenForumResponse.new
       @most_recent.key_proc = Proc.new { |key| key.to_sym }
@@ -31,23 +30,58 @@ module AmexOpenForum
   end
 
   def most_viewed(options = {})
-    @most_viewed ||= self.class.get("/v1/summaries/most-viewed", :query => build_query(options))
+    unless @most_viewed
+      @most_viewed          = OpenForumResponse.new
+      @most_viewed.key_proc = Proc.new { |key| key.to_sym }
+      returned_hash         = self.class.get("/v1/summaries/most-viewed", :query => build_query(options))["EntityListOfContent"]
+      returned_hash.each { |key, value| @most_viewed[key] = value}
+    end
+
+    @most_viewed
   end
 
   def most_commented(options = {})
-    @most_commented ||= self.class.get("/v1/summaries/most-commented", :query => build_query(options))
+    unless @most_commented
+      @most_commented           = OpenForumResponse.new
+      @most_commented.key_proc  = Proc.new { |key| key.to_sym }
+      returned_hash             = self.class.get("/v1/summaries/most-commented", :query => build_query(options))["EntityListOfContent"]
+      returned_hash.each { |key, value| @most_commented[key] = value}
+    end
+
+    @most_commented
   end
 
   def most_shared(options = {})
-    @most_shared ||= self.class.get("/v1/summaries/most-shared", :query => build_query(options))
+    unless @most_shared
+      @most_shared           = OpenForumResponse.new
+      @most_shared.key_proc  = Proc.new { |key| key.to_sym }
+      returned_hash          = self.class.get("/v1/summaries/most-shared", :query => build_query(options))["EntityListOfContent"]
+      returned_hash.each { |key, value| @most_shared[key] = value}
+    end
+
+    @most_shared
   end
 
   def article_details
+    unless @article_details
+      @article_details           = OpenForumResponse.new
+      @article_details.key_proc  = Proc.new { |key| key.to_sym }
+      returned_hash              = self.class.get("/v1/summaries/most-shared", :query => build_query(options))["EntityListOfContent"]
+      returned_hash.each { |key, value| @article_details[key] = value}
+    end
+
     @article_details ||= self.class.get("/v1/summaries/articles/details", :query => build_query(options))
   end
 
   def video_details
-    @video_details ||= self.class.get("/v1/summaries/videos/details", :query => build_query(options))
+    unless @video_details
+      @video_details           = OpenForumResponse.new
+      @video_details.key_proc  = Proc.new { |key| key.to_sym }
+      returned_hash            = self.class.get("/v1/summaries/videos/details", :query => build_query(options))["EntityListOfContent"]
+      returned_hash.each { |key, value| @video_details[key] = value}
+    end
+
+    @video_details
   end
 
 private
